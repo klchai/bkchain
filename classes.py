497c52d835c15ec6e48c70867cb23d84f1430841
Djaber Solimani <dso@affini-tech.com>
2021-02-05 14:25:09 +0100

    refactoring and add classes

---
+++ classes.py
@@ -0,0 +1,26 @@
+from hashlib import sha256
+
+class Block:
+    def __init__(self,previous_hash):
+        self.previous_hash = previous_hash
+        self.transactions = []
+
+class BlockChain:
+    def __init__(self):
+        self.blocks=[Block(None)]
+
+    def calculate_hash_of_block(self,block):
+        return sha256(block.encode())
+
+    def mine_block(self,block):
+        prev_hash=self.calculate_hash_of_block(self.blocks[-1])
+        block.previous_hash=prev_hash
+        self.blocks.append(block)
+
+class Transaction:
+    def __init__(self,payer,beneficiary,amount,timestamp):
+        self.payer = payer
+        self.beneficiary = beneficiary
+        self.amount = amount
+        self.timestamp = timestamp
+        
