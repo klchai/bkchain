d7bd55166f20e9c09a1511031080298149d42b34
klchai <chaikelun@gmail.com>
2021-03-21 12:21:10 +0100

    Wallet & Transaction

--- wallet.py
+++ wallet.py
@@ -3,45 +3,31 @@ import sys
 from hashlib import sha256
 from ecdsa import SigningKey, VerifyingKey
 from ecdsa.curves import SECP256k1
+from classes import Transaction
+import time
 
-if len(sys.argv)!=2:
-    print("Usage: python3 wallet.py miner_port")
-else:
-    miner_port = int(sys.argv[1])
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost",miner_port))
-    while True:
-        print("> ",end="")
-        cmd = input()
-        if cmd == "show":
-            msg="Wallet"
-            sock.send(msg.encode("ascii"))
-        elif cmd == "end":
-            sys.exit()
-        else:
-            print(f"Command '{cmd}' is incorrect")
-
-
-def applySig(prikey, inputs):
-    return prikey.sign(inputs)
-
-def verifySig(pubkey, data, signature):
-        return pubkey.verify(signature, data)
-
-class Wallet:
-    def __init__(self):
-        # un compte unique
-        self.private_key = SigningKey.generate(curve=SECP256k1)
-        self.public_key = self.private_key.get_verifying_key()
-
-    def get_pubkey(self):
-        return self.public_key.to_pem()
-        
-    def sign(self, msg):
-        h = sha256(msg.encode('utf8'))
-        return self.private_key.sign(h.digest())
-
-    def verifySignature(self, pubkey, msg, signature):
-        vf = VerifyingKey.from_pem(pubkey)
-        h = sha256(msg.encode('utf8'))
-        return vf.verify(signature, h.digest())
+def main():
+    if len(sys.argv)!=2:
+        print("Usage: python3 wallet.py miner_port")
+    else:
+        miner_port = int(sys.argv[1])
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("localhost",miner_port))
+        while True:
+            print("> ",end="")
+            cmd = input()
+            if cmd == "show":
+                msg="Wallet"
+                sock.send(msg.encode("ascii"))
+            if cmd == "transaction":
+                print("Your wallet ID:")
+                payer = input()
+                print("Destination wallet ID:")
+                beneficary = input()
+                print("The amount of transaction:")
+                amount = input()
+                msg = Transaction(payer, beneficary, amount, str(time.time()))
+            if cmd == "end":
+                sys.exit()
+            else:
+                print(f"Command '{cmd}' is incorrect")
